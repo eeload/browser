e304fd52a1fb6e94782638d56dc0661a75376218
mumuy <zhenghaole1990@126.com>
2026-01-15 20:21:18 +0800

    updated

--- src/module/system/iOS.js
+++ src/module/system/iOS.js
@@ -1,3 +1,4 @@
+import { version } from 'react';
 import userAgent from '../runtime/userAgent.js';
 
 export default {
@@ -7,5 +8,27 @@ export default {
             is:ua.includes('like Mac OS X'),
             version:ua.match(/OS ([\d_]+) like/)?.[1].replace(/_/g, '.')||''
         };
+    },
+    async is(){
+        let isMatch = this.parse().is;
+        if(!isMatch){
+            if (navigator.userAgentData && navigator.userAgentData.getHighEntropyValues) {
+                const uaData = await navigator.userAgentData.getHighEntropyValues([
+                    "platformVersion"
+                ]);
+                return uaData.platform === "iOS";
+            }
+        }
+        return isMatch;
+    },
+    async version() {
+        let version = this.parse().version;
+        if (navigator.userAgentData && navigator.userAgentData.getHighEntropyValues) {
+            const uaData = await navigator.userAgentData.getHighEntropyValues([
+                "platformVersion"
+            ]);
+            return uaData.platformVersion;
+        }
+        return version;
     }
 }
